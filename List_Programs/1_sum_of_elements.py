4ab1216aa3c2b02edb053f55bf4d67e46b4c6188
SushantSeven <sushant.das166@gmai.com>
2024-03-13 17:22:07 +0530

    [Sushant] Add: Python script to print the sum elements in a given list

---
+++ List_Programs/1_sum_of_elements.py
@@ -0,0 +1,20 @@
+# @Author: Sushant Das
+
+# @Date: 2024-03-13 
+
+# @Last Modified by: Author Name
+
+# @Last Modified time: 2021-02-11 18:00:30
+
+# @Title : Write a Python program to sum all the items in a list.
+
+# method to print all the elements of the list
+
+def list_sum(elements):
+    return sum(elements)
+
+def main():
+    print(list_sum([1,2,4,22,10]))
+
+if __name__ == "__main__":
+    main()

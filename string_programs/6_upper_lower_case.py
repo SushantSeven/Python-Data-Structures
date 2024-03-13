8b812635dab6bcce0d0e81d527ec572bd580655d
SushantSeven <sushant.das166@gmai.com>
2024-03-13 14:42:31 +0530

    [Sushant] Refactor: Added author name and date

--- string_programs/6_upper_lower_case.py
+++ string_programs/6_upper_lower_case.py
@@ -1,3 +1,13 @@
+# @Author: Sushant Das
+
+# @Date: 2024-03-13 
+
+# @Last Modified by: Author Name
+
+# @Last Modified time: 2021-02-11 18:00:30
+
+# @Title : Write a Python script that takes input from the user and displays that input back in upper and lower cases.
+
 # method to desiplay upper case and lower case
 def upper_lower(string):
     return string.upper(), string.lower()

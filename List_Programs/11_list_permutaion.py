6001fb320f243fa7f07a17596b48dab80f3c59a3
SushantSeven <sushant.das166@gmai.com>
2024-03-13 21:19:38 +0530

    [Sushant] Add: Display the permutations of a list

---
+++ List_Programs/11_list_permutaion.py
@@ -0,0 +1,25 @@
+# @Author: Sushant Das
+
+# @Date: 2024-03-13 
+
+# @Last Modified by: Author Name
+
+# @Last Modified time: 2021-02-11 18:00:30
+
+# @Title :  Write a Python program to generate all permutations of a list in Python.
+
+from itertools import permutations # importing permutations from itertools
+
+# method to display all the list permutations
+def list_permu(items):
+    new_list = list(permutations(items))
+    for item in new_list:
+        print(item)
+
+# main method
+def main():
+    list_permu(['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow'])
+
+
+if __name__ == "__main__":
+    main()

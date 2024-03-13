26fc01adb62b24b1da54eee3c0b3c1d58b40b42e
SushantSeven <sushant.das166@gmai.com>
2024-03-13 13:01:30 +0530

    [Sushant] Add: Python script to add ing or ly to the end of a given string

---
+++ string_programs/4_add_ing_or_ly.py
@@ -0,0 +1,17 @@
+# method to add ing or ly at the end of a string
+def add_ing_ly(string):
+    if len(string) >= 3:
+        if string.endswith("ing"):
+            return string+"ly"
+        else:
+            return string+"ing"
+    else:
+        return string
+
+# main method
+def main():
+    string = input("Enter the sring: ")
+    print(add_ing_ly(string))
+
+if __name__ == "__main__":
+    main()

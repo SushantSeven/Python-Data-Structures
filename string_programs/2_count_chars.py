2c4a29c789db3ed5ff190c7e0e79370b6c9ce051
SushantSeven <sushant.das166@gmai.com>
2024-03-13 12:46:51 +0530

    [Sushant] Refactor: Corrected the code

--- string_programs/2_count_chars.py
+++ string_programs/2_count_chars.py
@@ -1,15 +1,18 @@
 # 2. Write a Python program to count the number of characters (character frequency) in astring.
 
 # function to count the number of occurrence of a character in a string
-def count_characters(string, char):
-    return string.count(char)
-
+def count_characters(string):
+    new_string = "".join(string)
+    result_dic = {}
+    for i in string:
+        result_dic[i] = new_string.count(i)
+    return (result_dic)
+    
 # main function
 def main():
-    string = input("Enter the string: ")
-    char = input("Enter the character to be counted: ")
-    print(f"The number of occurrence of {char} in {string} is {count_characters(string,char)} times.")
-
+    string = list(input("Enter the string: "))
+    print(count_characters(string))
+    
 
 if __name__ == "__main__":
     main()

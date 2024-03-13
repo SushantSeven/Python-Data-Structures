ebad8049a7c5190f2a2361432ebb651215f551e1
SushantSeven <sushant.das166@gmai.com>
2024-03-13 14:41:52 +0530

    [Sushant] Refactor: Added author name and date

--- string_programs/3_change_to_$.py
+++ string_programs/3_change_to_$.py
@@ -1,6 +1,15 @@
-# 3. Write a Python program to get a string from a given string where all occurrences of its
-# first char have been changed to '$', except the first char itself.
+# @Author: Sushant Das
 
+# @Date: 2024-03-13 
+
+# @Last Modified by: Author Name
+
+# @Last Modified time: 2021-02-11 18:00:30
+
+# @Title : Write a Python program to get a string from a given string where all occurrences of its
+#          first char have been changed to '$', except the first char itself.
+
+# method to replace 
 def replace_char(string):    
     new_string = string.lower()
     ls =[]

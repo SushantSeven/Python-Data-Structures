92022c9ce528a7728ed8e2371fd29fa66b1309c1
SushantSeven <sushant.das166@gmai.com>
2024-03-13 14:42:06 +0530

    [Sushant] Refactor: Added author name and date

--- string_programs/4_add_ing_or_ly.py
+++ string_programs/4_add_ing_or_ly.py
@@ -1,3 +1,16 @@
+# @Author: Sushant Das
+
+# @Date: 2024-03-13 
+
+# @Last Modified by: Author Name
+
+# @Last Modified time: 2021-02-11 18:00:30
+
+# @Title : Write a Python program to add 'ing' at the end of a given string (length should be at
+#           least 3). If the given string already ends with 'ing' then add 'ly' instead. If the string length of the
+
+#           given string is less than 3, leave it unchanged.
+
 # method to add ing or ly at the end of a string
 def add_ing_ly(string):
     if len(string) >= 3:

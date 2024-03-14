ca418cf3f8dc316e0ef6c228948b14d2f2771483
SushantSeven <sushant.das166@gmai.com>
2024-03-14 12:31:54 +0530

    [Sushant] Refactor: Added proper print statements to the code

--- List_Programs/13_append_list.py
+++ List_Programs/13_append_list.py
@@ -14,7 +14,10 @@ def append_list(list1, list2):
 
 # main method
 def main():
-    print(append_list([5,7,10,11],[1,22,3,4,2,6,7,9,0]))
+    ls1 = [5,7,10,11]
+    ls2 = [1,22,3,4,2,6,7,9,0]
+    print(f"The original lists are: {ls1} {ls2}")
+    print(f"The appended list is: {append_list(ls1,ls2)}")
 
 
 if __name__ == "__main__":

49dde77f87d8f7206e034509c7233e30037fd755
SushantSeven <sushant.das166@gmai.com>
2024-03-14 12:34:04 +0530

    [Sushant] Refactor: Added proper print statements to the code

--- List_Programs/15_common_items.py
+++ List_Programs/15_common_items.py
@@ -18,7 +18,10 @@ def append_list(list1, list2):
 
 # main method
 def main():
-    print(append_list([1,22,3,4,2,6,7,9,0],[1,7,9,7,10,11]))
+    ls1 = [1,22,3,4,2,6,7,9,0]
+    ls2 = [1,7,9,7,10,11]
+    print(f"The original lists are: {ls1} {ls2}")
+    print(f"The common items are: {append_list(ls1,ls2)}")
 
 
 if __name__ == "__main__":
